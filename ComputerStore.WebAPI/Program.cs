aec65b920391daef33a0645c0332dd4f980e71b4
miloshevska-m <131915376+miloshevska-m@users.noreply.github.com>
2025-05-13 19:13:47 +0200

    Update Program.cs

--- ComputerStore.WebAPI/Program.cs
+++ ComputerStore.WebAPI/Program.cs
@@ -10,10 +10,9 @@ using ComputerStore.services.Mapping;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ComputerStoreContext>(options =>
@@ -31,7 +30,6 @@ builder.Services.AddAutoMapper(typeof(mappingprofile));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
